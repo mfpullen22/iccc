e9cb9edd669eb07c2efa76dc96b1f0f6243faaaa
Matthew Pullen <matthew.pullen@gmail.com>
2022-12-27 23:17:58 -0500

    Trying to get iOS to work

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+//import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:flutter/material.dart';
 //import 'package:flutterfire_ui/auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
